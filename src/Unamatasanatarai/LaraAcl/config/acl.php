91f7ecb26fb6005d43672daccbaee5ae397befe0
Piotr Grabowski <piograski@gmail.com>
2017-03-31 12:37:33 +0200

    qf

--- src/Unamatasanatarai/LaraAcl/config/acl.php
+++ src/Unamatasanatarai/LaraAcl/config/acl.php
@@ -2,17 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Models
-    |--------------------------------------------------------------------------
-    |
-    | If you want, you can replace default models from this package by models
-    | you created. Have a look at `Ultraware\Roles\Models\Role` model and
-    | `Ultraware\Roles\Models\Permission` model.
-    |
-    */
-
     'providers' => [
         'permissions' => [
             'model' => \Unamatasanatarai\LaraAcl\Models\Permission::class,
