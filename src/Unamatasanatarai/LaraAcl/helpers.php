1d2852514ff1369433011194fd7bfddb6c34caf5
Piotr Grabowski <piotr.grabowski@interak.pl>
2017-11-15 08:31:00 +0100

    autoload helper method

--- src/Unamatasanatarai/LaraAcl/helpers.php
+++ src/Unamatasanatarai/LaraAcl/helpers.php
@@ -1,10 +1,9 @@
 <?php
 if ( ! function_exists('permitted_if')) {
     function permitted_if($permissions) {
-        if (\Auth::user() && \Auth::user()->hasPermission($permissions))
+        if ( ! \Auth::user() || ! \Auth::user()->hasPermission($permissions))
         {
-            return true;
+            abort(401);
         }
-        abort(404);
     }
 }
